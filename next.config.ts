16d03fffa1b1904462d83c9866fe22969002e926
zezuxxxeox <zezun2618@gmail.com>
2026-06-17 15:54:23 +0900

    feat:feat: hide Next.js indicator in development mode

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // 개발 모드 좌측 하단의 Next.js 표시(동그라미) 숨김
+  devIndicators: false,
 };
 
 export default nextConfig;
